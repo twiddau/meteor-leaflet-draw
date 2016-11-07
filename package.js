40cefd9971c7bba4b497773e586ca71a8d3d024d
Twi <git@twiddau.de>
2016-11-07 22:30:15 +0100

    Update leaflet-draw to 0.4.1
    Update leaflet-draw to the most recent version

--- package.js
+++ package.js
@@ -1,18 +1,19 @@
 Package.describe({
-    name: "bdunnette:leaflet-draw",
+    name: "twiddau:leaflet-draw",
     summary: "Adds support for drawing and editing vectors and markers on Leaflet maps",
-    version: "2.0.0",
-    git: "https://github.com/bdunnette/meteor-leaflet-draw.git"
+    version: "0.4.1-0",
+    git: "https://github.com/twiddau/meteor-leaflet-draw.git"
 });
 
 Package.onUse(function(api) {
-    api.use('bevanhunt:leaflet@2.0.0', 'client');
+    api.use(':leaflet@3.0.1', 'client');
     api.addFiles([
         'lib/leaflet.draw.js',
         'leaflet.draw.css'
     ], 'client');
     api.addAssets([
         'images/spritesheet.png',
-        'images/spritesheet-2x.png'
+        'images/spritesheet-2x.png',
+		'images/spritesheet.svg'
     ], 'client');
 });
